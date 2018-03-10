352435e1495e414ec8ea1dc61f2199135cba4b34
Antony Ho <ntonyworkshop@gmail.com>
2018-03-11 02:30:44 +0800

    Adding get_timeseries and refactoring API URL path

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #!eikonrest/bin/python
-from flask import Flask, request, jsonify
+from functools import wraps
+from flask import Flask, request, Response, jsonify
 from flask_restful import Resource, Api
 # from json import dumps
 import eikon as ek
@@ -11,25 +12,39 @@ conf = yaml.load(open('config.yml'))
 ek.set_app_id(conf['eikon']['apikey'])
 
 
-class Query(Resource):
-    def get(self, syms, fs, params=''):
+def requires_auth(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
         #  Check UUID in Bearer of API request. This is a simple wrapper.
         token = request.headers.get('Authorization', None)
         if token is None:
-            return None
+            return Response('Not authenticated', 401, {'WWWAuthenticate':'Token realm="Authentication Required"'})
         method, tokenstr = token.split()
         if method.lower() == "bearer" and tokenstr == conf['pywrapper']['apikey']:
-            df = ek.get_data(instruments=str(syms).split(','), fields=str(fs).split(','), parameters=params,
-                             raw_output=True)
-            return jsonify(df)
+            return f(*args, **kwargs)
         else:
-            return None
+            return Response('Not authenticated', 401, {'WWWAuthenticate':'Token realm="Authentication Required"'})
+    return decorated
+
+class Query(Resource):
+    @requires_auth
+    def get(self, syms, fs, params=''):
+        df = ek.get_data(instruments=str(syms).split(','), fields=str(fs).split(','), parameters=params,
+                         raw_output=True)
+        return jsonify(df)
+
+class Interval(Resource):
+    def get(self, syms, fs, start, end, interval):
+        df = ek.get_timeseries(rics=str(syms).split(','), start_date=start, end_date=end, interval="weekly", fields=fs,
+                               calendar="tradingdays", raw_output=True)
+        return jsonify(df)
 
 
 # Example: To search with a wildcard HK Heng Seng Index symbol '0#HSI*.HF'
-# /query/0%23HSI*.HF/PUT_CALL,CF_LAST,CF_ASK,CF_BID,PCTCHNG
+# /get_data/0%23HSI*.HF/PUT_CALL,CF_LAST,CF_ASK,CF_BID,PCTCHNG
 # Use %23 to replace pound '#' as it represents anchor/bookmark on URL
-api.add_resource(Query, '/query/<string:syms>/<string:fs>', '/query/<string:syms>/<string:fs>/<string:params>')
+api.add_resource(Query, '/get_data/<string:syms>/<string:fs>', '/query/<string:syms>/<string:fs>/<string:params>')
+api.add_resource(Interval, '/get_timeseries/<string:syms>/<string:fs>/<string:start>/<string:end>/<string:interval>/<string:calendar>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1368)
