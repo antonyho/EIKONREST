93bf58aeed11fb4d2ba0de1c22c61fe010731223
Antony Ho <ntonyworkshop@gmail.com>
2018-03-11 10:59:44 +0800

    Tweaking URL and query parameters and fixing data type bugs

--- server.py
+++ server.py
@@ -34,9 +34,13 @@ class Query(Resource):
         return jsonify(df)
 
 class Interval(Resource):
-    def get(self, syms, fs, start, end, interval='weekly'):
-        df = ek.get_timeseries(rics=str(syms).split(','), start_date=start, end_date=end, interval=interval, fields=fs,
-                               calendar="tradingdays", raw_output=True)
+    @requires_auth
+    def get(self, syms, start, end, fs='*', itrvl='weekly'):
+        calc=request.args.get('calc')
+        if calc is None or calc == '':
+            calc='tradingdays'
+        df = ek.get_timeseries(rics=str(syms).split(','), start_date=start, end_date=end, interval=str(itrvl),
+                               fields=str(fs).split(','), calendar=calc, raw_output=True)
         return jsonify(df)
 
 
@@ -44,7 +48,8 @@ class Interval(Resource):
 # /get_data/0%23HSI*.HF/PUT_CALL,CF_LAST,CF_ASK,CF_BID,PCTCHNG
 # Use %23 to replace pound '#' as it represents anchor/bookmark on URL
 api.add_resource(Query, '/get_data/<string:syms>/<string:fs>', '/query/<string:syms>/<string:fs>/<string:params>')
-api.add_resource(Interval, '/get_timeseries/<string:syms>/<string:fs>/<string:start>/<string:end>/<string:interval>/<string:calendar>')
+api.add_resource(Interval, '/get_timeseries/<string:syms>/<string:start>/<string:end>/<string:fs>',
+                 '/get_timeseries/<string:syms>/<string:start>/<string:end>/<string:fs>/<string:itrvl>')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1368)
